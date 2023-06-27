49485b974fd0877000c8199dbe62fb648f4f770a
Anusha Gaargya <anushasgarg@gmail.com>
2023-06-27 17:34:22 +0530

    routing requests

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 const http = require('http');
 
 const server = http.createServer((req, res)=>{
-console.log(req.url,req.method,req.headers)
+const url = req.url;
+if (url==='/home'){
 res.setHeader('Content-Type', 'text/html');
 res.write('<html');
 res.write('<head><title>Anusha S</title></head>')
-res.write('<body>Hello, This is Anusha</body>')
+res.write('<body>Welcome Home</body>')
 res.write('<html>')
-res.end();
+res.end();}
+
+else if (url==='/about'){
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html');
+    res.write('<head><title>Anusha S</title></head>')
+    res.write('<body>Welcome to About Us Page</body>')
+    res.write('<html>')
+    res.end();}
+
+else if (url==='/node'){
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html');
+        res.write('<head><title>Anusha S</title></head>')
+        res.write('<body>Welcome to my Node.js Project</body>')
+        res.write('<html>')
+        res.end();}
+
 
 });
 
